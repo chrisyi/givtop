a3d87ea0ec312b66b258f502585de4d2e02e0c39
cy <cy@Christophers-MacBook-Pro.local>
2017-08-30 20:51:43 -0500

    continuing with reddit api only

--- master/client/src/App.js
+++ master/client/src/App.js
@@ -1,29 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
 import SearchBar from "./components/search_bar";
-import YTSearch from "youtube-search";
-import RedditAPI from "./components/reddit_api";
-import ImgurAPI from "./components/imgur_api"
-
-// Youtube API
-const API_KEY = "AIzaSyBbcx_owAq66fzSPpBtWqBFWR55EsdUY2E";
-const currentDate = new Date(Date.now()).toISOString();
-const dateToGoogle = () => {
-  return currentDate.substring(0, currentDate.indexOf("T")) + "T05:00:00Z";
-};
-const YTOptions = {
-  maxResults: 5,
-  key: API_KEY,
-  order: "viewCount",
-  publishedAfter: dateToGoogle(),
-  safeSearch: "moderate",
-  type: "video"
-};
-YTSearch("music", YTOptions, (err, results) => {
-  if (err) return console.log(err);
-  console.log(results);
-});
-//  end Youtube API
+// import YTSearch from "youtube-search";
+// import ImgurAPI from "./components/imgur_api"
+import RedditAPI from "./components/reddit-api-search";
+import RedditList from "./components/reddit_list"
 
 class App extends Component {
 
@@ -32,6 +13,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header" />
         <SearchBar />
+        <RedditList />
         <RedditAPI redditQuery={"cat"} />
       </div>
     );
@@ -39,3 +21,24 @@ class App extends Component {
 }
 
 export default App;
+
+
+// Youtube API
+// const API_KEY = "AIzaSyBbcx_owAq66fzSPpBtWqBFWR55EsdUY2E";
+// const currentDate = new Date(Date.now()).toISOString();
+// const dateToGoogle = () => {
+//   return currentDate.substring(0, currentDate.indexOf("T")) + "T05:00:00Z";
+// };
+// const YTOptions = {
+//   maxResults: 5,
+//   key: API_KEY,
+//   order: "viewCount",
+//   publishedAfter: dateToGoogle(),
+//   safeSearch: "moderate",
+//   type: "video"
+// };
+// YTSearch("music", YTOptions, (err, results) => {
+//   if (err) return console.log(err);
+//   console.log(results);
+// });
+//  end Youtube API
---
+++ master/client/src/components/reddit-api-search.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class RedditAPI extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redditPosts: []
+    };
+
+    this.redditUrlRoot = `http://www.reddit.com/search.json?q=title%3A${this
+      .props.redditQuery}&limit=6&t=day&restrict_sr=true&sort=top`;
+  }
+
+  componentDidMount() {
+    axios.get(this.redditUrlRoot).then(res => {
+      console.log(res);
+      const redditPosts = res.data.data.children.map(obj => obj.data);
+      this.setState({ redditPosts });
+      console.log(this.state.redditPosts);
+    });
+  }
+
+  //   <ul>
+  //   {this.state.redditPosts.map(post =>
+  //     <li key={post.id}>
+  //       {post.title}
+  //     </li>
+  //   )}
+  // </ul>
+
+  render() {
+    return <div />;
+  }
+}
+
+export default RedditAPI;
